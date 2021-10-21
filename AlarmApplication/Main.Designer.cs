c9b8cfa0621a94e7cfdaeb8048f006c904c6c8df
Hikmat Samadov <hikmetsemedov0177@gmail.com>
2021-10-21 10:00:28 +0400

    Second Version

--- AlarmApplication/Main.Designer.cs
+++ AlarmApplication/Main.Designer.cs
@@ -40,32 +40,35 @@ namespace AlarmApplication
             this.btn_stop = new System.Windows.Forms.Button();
             this.timer_alarm = new System.Windows.Forms.Timer(this.components);
             this.btn_snooze = new System.Windows.Forms.Button();
+            this.cb_alarmsound = new System.Windows.Forms.ComboBox();
+            this.lbl_alarmsound = new System.Windows.Forms.Label();
+            this.lbl_snoozetime = new System.Windows.Forms.Label();
+            this.tb_snoozetime = new System.Windows.Forms.TextBox();
+            this.tb_console = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lbl_currenttime
             // 
             this.lbl_currenttime.AutoSize = true;
             this.lbl_currenttime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_currenttime.Location = new System.Drawing.Point(255, 150);
+            this.lbl_currenttime.Location = new System.Drawing.Point(571, 56);
             this.lbl_currenttime.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbl_currenttime.Name = "lbl_currenttime";
             this.lbl_currenttime.Size = new System.Drawing.Size(117, 24);
             this.lbl_currenttime.TabIndex = 3;
             this.lbl_currenttime.Text = "Current time:";
-            this.lbl_currenttime.Click += new System.EventHandler(this.lbl_currenttime_Click);
             // 
             // tb_currenttime
             // 
             this.tb_currenttime.Enabled = false;
             this.tb_currenttime.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_currenttime.Location = new System.Drawing.Point(375, 148);
+            this.tb_currenttime.Location = new System.Drawing.Point(700, 49);
             this.tb_currenttime.Margin = new System.Windows.Forms.Padding(6);
             this.tb_currenttime.Name = "tb_currenttime";
             this.tb_currenttime.ReadOnly = true;
             this.tb_currenttime.Size = new System.Drawing.Size(144, 31);
             this.tb_currenttime.TabIndex = 2;
             this.tb_currenttime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.tb_currenttime.TextChanged += new System.EventHandler(this.tb_currenttime_TextChanged);
             // 
             // timer_currenttime
             // 
@@ -75,7 +78,7 @@ namespace AlarmApplication
             // tb_alarmtime
             // 
             this.tb_alarmtime.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_alarmtime.Location = new System.Drawing.Point(375, 191);
+            this.tb_alarmtime.Location = new System.Drawing.Point(700, 92);
             this.tb_alarmtime.Margin = new System.Windows.Forms.Padding(6);
             this.tb_alarmtime.Name = "tb_alarmtime";
             this.tb_alarmtime.Size = new System.Drawing.Size(144, 31);
@@ -86,18 +89,17 @@ namespace AlarmApplication
             // 
             this.lbl_alarmtime.AutoSize = true;
             this.lbl_alarmtime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_alarmtime.Location = new System.Drawing.Point(267, 194);
+            this.lbl_alarmtime.Location = new System.Drawing.Point(584, 99);
             this.lbl_alarmtime.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbl_alarmtime.Name = "lbl_alarmtime";
             this.lbl_alarmtime.Size = new System.Drawing.Size(104, 24);
             this.lbl_alarmtime.TabIndex = 5;
             this.lbl_alarmtime.Text = "Alarm time:";
-            this.lbl_alarmtime.Click += new System.EventHandler(this.lbl_alarmtime_Click);
             // 
             // btn_setalarm
             // 
             this.btn_setalarm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_setalarm.Location = new System.Drawing.Point(330, 243);
+            this.btn_setalarm.Location = new System.Drawing.Point(716, 374);
             this.btn_setalarm.Name = "btn_setalarm";
             this.btn_setalarm.Size = new System.Drawing.Size(128, 49);
             this.btn_setalarm.TabIndex = 6;
@@ -108,7 +110,7 @@ namespace AlarmApplication
             // btn_stop
             // 
             this.btn_stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_stop.Location = new System.Drawing.Point(259, 413);
+            this.btn_stop.Location = new System.Drawing.Point(601, 374);
             this.btn_stop.Name = "btn_stop";
             this.btn_stop.Size = new System.Drawing.Size(87, 49);
             this.btn_stop.TabIndex = 7;
@@ -123,7 +125,7 @@ namespace AlarmApplication
             // 
             // btn_snooze
             // 
-            this.btn_snooze.Location = new System.Drawing.Point(408, 413);
+            this.btn_snooze.Location = new System.Drawing.Point(733, 178);
             this.btn_snooze.Name = "btn_snooze";
             this.btn_snooze.Size = new System.Drawing.Size(111, 49);
             this.btn_snooze.TabIndex = 8;
@@ -131,13 +133,71 @@ namespace AlarmApplication
             this.btn_snooze.UseVisualStyleBackColor = true;
             this.btn_snooze.Click += new System.EventHandler(this.btn_snooze_Click);
             // 
+            // cb_alarmsound
+            // 
+            this.cb_alarmsound.FormattingEnabled = true;
+            this.cb_alarmsound.Items.AddRange(new object[] {
+            "Default",
+            "Radar",
+            "Morning"});
+            this.cb_alarmsound.Location = new System.Drawing.Point(700, 132);
+            this.cb_alarmsound.Name = "cb_alarmsound";
+            this.cb_alarmsound.Size = new System.Drawing.Size(144, 32);
+            this.cb_alarmsound.TabIndex = 9;
+            // 
+            // lbl_alarmsound
+            // 
+            this.lbl_alarmsound.AutoSize = true;
+            this.lbl_alarmsound.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_alarmsound.Location = new System.Drawing.Point(566, 140);
+            this.lbl_alarmsound.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_alarmsound.Name = "lbl_alarmsound";
+            this.lbl_alarmsound.Size = new System.Drawing.Size(122, 24);
+            this.lbl_alarmsound.TabIndex = 10;
+            this.lbl_alarmsound.Text = "Alarm sound:";
+            // 
+            // lbl_snoozetime
+            // 
+            this.lbl_snoozetime.AutoSize = true;
+            this.lbl_snoozetime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_snoozetime.Location = new System.Drawing.Point(497, 190);
+            this.lbl_snoozetime.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_snoozetime.Name = "lbl_snoozetime";
+            this.lbl_snoozetime.Size = new System.Drawing.Size(151, 24);
+            this.lbl_snoozetime.TabIndex = 12;
+            this.lbl_snoozetime.Text = "Snooze minutes:";
+            // 
+            // tb_snoozetime
+            // 
+            this.tb_snoozetime.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_snoozetime.Location = new System.Drawing.Point(660, 188);
+            this.tb_snoozetime.Margin = new System.Windows.Forms.Padding(6);
+            this.tb_snoozetime.Name = "tb_snoozetime";
+            this.tb_snoozetime.Size = new System.Drawing.Size(57, 31);
+            this.tb_snoozetime.TabIndex = 11;
+            this.tb_snoozetime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb_console
+            // 
+            this.tb_console.Location = new System.Drawing.Point(12, 283);
+            this.tb_console.Multiline = true;
+            this.tb_console.Name = "tb_console";
+            this.tb_console.Size = new System.Drawing.Size(263, 256);
+            this.tb_console.TabIndex = 13;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = global::AlarmApplication.Properties.Resources.clock;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(850, 598);
+            this.Controls.Add(this.tb_console);
+            this.Controls.Add(this.lbl_snoozetime);
+            this.Controls.Add(this.tb_snoozetime);
+            this.Controls.Add(this.lbl_alarmsound);
+            this.Controls.Add(this.cb_alarmsound);
             this.Controls.Add(this.btn_snooze);
             this.Controls.Add(this.btn_stop);
             this.Controls.Add(this.btn_setalarm);
@@ -168,6 +228,11 @@ namespace AlarmApplication
         private System.Windows.Forms.Button btn_stop;
         private System.Windows.Forms.Timer timer_alarm;
         private System.Windows.Forms.Button btn_snooze;
+        private System.Windows.Forms.ComboBox cb_alarmsound;
+        private System.Windows.Forms.Label lbl_alarmsound;
+        private System.Windows.Forms.Label lbl_snoozetime;
+        private System.Windows.Forms.TextBox tb_snoozetime;
+        private System.Windows.Forms.TextBox tb_console;
     }
 }
 
--- AlarmApplication/Main.cs
+++ AlarmApplication/Main.cs
@@ -16,51 +16,71 @@ namespace AlarmApplication
         public Main()
         {
             InitializeComponent();
+            this.Text = "Alarm " + DateTime.Now.ToLongTimeString();
         }
+
         SoundPlayer sp;
+
         private void Main_Load(object sender, EventArgs e)
         {
             timer_currenttime.Start();
             tb_alarmtime.Text = DateTime.Now.ToLongTimeString();
         }
 
+
         private void timer_currenttime_Tick(object sender, EventArgs e)
         {
             tb_currenttime.Text = DateTime.Now.ToLongTimeString();
         }
 
-        private void btn_setalarm_Click(object sender, EventArgs e)
-        {
-            timer_alarm.Start();
-        }
-
+        
         private void timer_alarm_Tick(object sender, EventArgs e)
         {
             if (tb_currenttime.Text == tb_alarmtime.Text)
             {
-
-                string soundPath = Application.StartupPath + "\\music\\alarm1.wav";
-                //MessageBox.Show(soundPath);
+                string sound_file_name = cb_alarmsound.Text.ToLower();
+                string soundPath = Application.StartupPath + "\\music\\" + sound_file_name + ".wav";
                 sp = new SoundPlayer(soundPath);
                 sp.Play();
                 timer_alarm.Stop();
-                //MessageBox.Show("Alarm is on! Time to wake up!");
             }
 
         }
+
+        private void timer_snoozetime_Tick(object sender, EventArgs e)
+        {
+            tb_snoozetime.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void btn_setalarm_Click(object sender, EventArgs e)
+        {
+            timer_alarm.Start();
+            Console.WriteLine("Set Alarm " + DateTime.Now);
+            tb_console.AppendText("[ " + DateTime.Now.ToLongTimeString() + " ] Alarm is set!\r\n");
+        }
+
 
         private void btn_stop_Click(object sender, EventArgs e)
         {
             timer_alarm.Stop();
             sp.Stop();
+            Console.WriteLine("Stop " + DateTime.Now);
+            tb_console.AppendText("[ " + DateTime.Now.ToLongTimeString() + " ] Alarm is stopped!\r\n");
         }
 
         private void btn_snooze_Click(object sender, EventArgs e)
         {
             timer_alarm.Stop();
             sp.Stop();
-            string old_alarm_time = tb_alarmtime.Text;
-            MessageBox.Show(old_alarm_time);
+            // string old_alarm_time = tb_alarmtime.Text;
+            string snooze_time = tb_snoozetime.Text;
+            int snooze_time_int = Int32.Parse(snooze_time);
+            string new_alarm_time = DateTime.Now.AddMinutes(snooze_time_int).ToLongTimeString();
+            tb_alarmtime.Text = new_alarm_time;
+            timer_alarm.Start();
+            Console.WriteLine("Snooze " + DateTime.Now);
+            tb_console.AppendText("[ " + DateTime.Now.ToLongTimeString() + " ] Alarm is snoozed!\r\n");
         }
+
     }
 }
